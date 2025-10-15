af2ab03444e89565237b62e8f336e349451d8d79
Quy <96563375+QuyTurtle12@users.noreply.github.com>
2025-10-15 16:07:20 +0700

    Update ContestDbContext.cs

--- InnoCode_Challenge/DataAccess/Entities/ContestDbContext.cs
+++ InnoCode_Challenge/DataAccess/Entities/ContestDbContext.cs
@@ -79,10 +79,6 @@ public partial class ContestDbContext : DbContext
 
     public virtual DbSet<User> Users { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=tcp:fa25se215.database.windows.net,1433;Initial Catalog=ContestDB;Persist Security Info=False;User ID=fa25se215;Password=C@pstone;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<ActivityLog>(entity =>
