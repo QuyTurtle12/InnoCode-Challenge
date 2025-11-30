ef97fd4161af414af94c8f19d7456c0b55828955
Quy <96563375+QuyTurtle12@users.noreply.github.com>
2025-11-30 17:42:54 +0700

    chore: add type to round problem config

--- InnoCode_Challenge/Repository/DTOs/ProblemDTOs/GetProblemDTO.cs
+++ InnoCode_Challenge/Repository/DTOs/ProblemDTOs/GetProblemDTO.cs
@@ -1,10 +1,14 @@
-﻿namespace Repository.DTOs.ProblemDTOs
+﻿using Utility.Enums;
+
+namespace Repository.DTOs.ProblemDTOs
 {
     public class GetProblemDTO : BaseProblemDTO
     {
         public Guid ProblemId { get; set; }
 
         public string? TemplateUrl { get; set; }
+
+        public string? Type { get; set; }
 
         public DateTime CreatedAt { get; set; }
     }
