ee450dcc3c4e5fe6ddbc9dca651558f0182b1d3d
Quy <96563375+QuyTurtle12@users.noreply.github.com>
2026-01-25 12:10:54 +0700

    chore: remove retry logic

--- InnoCode_Challenge/InnoCode_Challenge_API/DI/DependencyInjection.cs
+++ InnoCode_Challenge/InnoCode_Challenge_API/DI/DependencyInjection.cs
@@ -256,13 +256,7 @@ namespace InnoCode_Challenge_API.DI
         {
             services.AddDbContext<ContestDbContext>(options =>
             {
-                options.UseSqlServer(
-                    configuration.GetConnectionString("MyCnn"),
-                    sqlServerOptions => sqlServerOptions.EnableRetryOnFailure(
-                        maxRetryCount: 5,
-                        maxRetryDelay: TimeSpan.FromSeconds(30),
-                        errorNumbersToAdd: null
-                    ));
+                options.UseSqlServer(configuration.GetConnectionString("MyCnn"));
             });
         }
 
